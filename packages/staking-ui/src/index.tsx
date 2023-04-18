72077a11fdd388354222879fb0dcd642ef49ddf3
Vasin Sermsampan <dev.vasin@gmail.com>
2023-04-18 10:53:27 +0700

    feat(*): add validator & sentry.io (#28)

--- packages/staking-ui/src/index.tsx
+++ packages/staking-ui/src/index.tsx
@@ -10,6 +10,19 @@ import "./assets/css/input.css";
 import "./assets/css/pagination.css";
 import Main from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://081561c4434a452fb0fef86b569ec129@o465173.ingest.sentry.io/4505033037053952",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 1.0 : 0.1, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: process.env.NODE_ENV === "production" ? 0.0 : 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  environment: process.env.NODE_ENV,
+  attachStacktrace: true,
+});
 
 const App = () => {
   return (
